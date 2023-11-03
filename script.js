18de44e9ab6e0fb4da1473a30bdf6fcd65791e45
Calculus <tahar.ayoob@gmail.com>
2023-11-02 19:36:53 -0700

    Proof of concept

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const result = document.querySelector('#preview');
-
+const preview = document.querySelector('#preview');
 for (const option of document.querySelectorAll('input[name="head"]')) {
 	option.addEventListener('click', event => {
 		updatePreview();
@@ -14,8 +13,13 @@ for (const option of document.querySelectorAll('input[name="hat"]')) {
 
 updatePreview();
 
-function updatePreview() {
-	let head = document.querySelector('input[name="head"]:checked + svg');
-	let hat = document.querySelector('input[name="hat"]:checked + svg');
-	console.log('hello', head, hat)
+async function updatePreview() {
+	let parser = new DOMParser();
+	let head = document.querySelector('input[name="head"]:checked + img');
+	let hat = document.querySelector('input[name="hat"]:checked + img');
+	let combined = [];
+	head && (combined.push(...await fetch(head.getAttribute('src')).then(r => r.text()).then(r => parser.parseFromString(r, 'image/svg+xml')).then(r => r.querySelector('svg').children)));
+	hat && (combined.push(...await fetch(hat.getAttribute('src')).then(r => r.text()).then(r => parser.parseFromString(r, 'image/svg+xml')).then(r => r.querySelector('svg').children)));
+	console.log('hello', head, hat, combined)
+	preview.replaceChildren(...combined);
 }
